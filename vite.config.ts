2baa3cc916690f39b07bc7633f90b96914df0811
irfansofyana <irfansofyana0305@gmail.com>
2024-12-18 22:36:01 +0700

    feat: github action deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/inez-birthday/', // Add base URL for GitHub Pages
+  base: '/inez-birthday/',  // Update to match repository name under irfansofyana
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
