a360586ddbd7c2238f1b388126238d7877ee19c3
irfansofyana <irfansofyana0305@gmail.com>
2024-12-20 15:27:41 +0700

    fix: attempt to fix after custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/inez-birthday/',  // Update to match repository name under irfansofyana
+  base: '/',  // Changed to root for custom domain
   publicDir: 'public',
   optimizeDeps: {
     exclude: ['lucide-react'],
